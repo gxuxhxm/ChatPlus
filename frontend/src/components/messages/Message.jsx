af5af250b118e52fffea438cacec1d3c9be502c2
gxuxhxm <gxuxhxm@gmail.com>
2024-07-11 23:30:43 +0530

    connected Frontend with backend (lil bit of socket too)

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import { useAuthContext } from "../../context/AuthContext";
+import { extractTime } from "../../utils/extractTime";
+import useConversation from "../../zustand/useConversation";
 
-const Message = () => {
+const Message = ({ message }) => {
+  const { authUser } = useAuthContext();
+  const { selectedConversation } = useConversation();
+  const fromMe = message.senderId === authUser._id;
+  const formattedTime = extractTime(message.createdAt);
+  const chatClassName = fromMe ? "chat-end" : "chat-start";
+  const profilePic = fromMe
+    ? authUser.profilePic
+    : selectedConversation?.profilePic;
+  const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+
   return (
-    <div className='chat chat-end'>
-        <div className='chat-image avatar'>
-				<div className='w-10 rounded-full'>
-					<img alt='Tailwind CSS chat bubble component' src='https://i.pinimg.com/originals/76/08/41/76084173d1c00cf755a3530aa422bb2e.gif' />
-				</div>
-			</div>
-			<div className={`chat-bubble text-white bg-blue-500`}>Hi whatsup bro! the winter is coming</div>
-			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'> 12:12</div>
-		</div>
-  )
-}
-
-export default Message
+    <div className={`chat ${chatClassName}`}>
+      <div className="chat-image avatar">
+        <div className="w-10 rounded-full">
+          <img alt="Tailwind CSS chat bubble component" src={profilePic} />
+        </div>
+      </div>
+      <div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>
+        {message.message}
+      </div>
+      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
+        {formattedTime}
+      </div>
+    </div>
+  );
+};
+export default Message;
--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,24 +1,62 @@
-import React from 'react'
+import useConversation from "../../zustand/useConversation";
 
-const Conversation = () => {
-  return <>
-			<div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer'>
-				<div className='avatar online'>
-					<div className='w-12 rounded-full'>
-						<img src='https://media.tenor.com/zZOt7alSzAMAAAAM/gojo-gojo-satoru.gif' alt='user avatar' />
-					</div>
-				</div>
+const Conversation = ({ conversation, lastIdx, emoji }) => {
+  const { selectedConversation, setSelectedConversation } = useConversation();
 
-				<div className='flex flex-col flex-1'>
-					<div className='flex gap-3 justify-between'>
-						<p className='font-bold text-gray-200'>John doe</p>
-						<span className='text-xl'>☃️</span>
-					</div>
-				</div>
-			</div>
+  const isSelected = selectedConversation?._id === conversation._id;
 
-    <div className='divider my-0 py-0 h-1' />
-		</>
-}
+  return (
+    <>
+      <div
+        className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
+				${isSelected ? "bg-sky-500" : ""}
+			`}
+        onClick={() => setSelectedConversation(conversation)}
+      >
+        <div className="avatar online">
+          <div className="w-12 rounded-full">
+            <img src={conversation.profilePic} alt="user avatar" />
+          </div>
+        </div>
 
+        <div className="flex flex-col flex-1">
+          <div className="flex gap-3 justify-between">
+            <p className="font-bold text-gray-200">{conversation.fullName}</p>
+            <span className="text-xl">{emoji}</span>
+          </div>
+        </div>
+      </div>
+
+      {!lastIdx && <div className="divider my-0 py-0 h-1" />}
+    </>
+  );
+};
 export default Conversation;
+
+// STARTER CODE SNIPPET
+// const Conversation = () => {
+// 	return (
+// 		<>
+// 			<div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer'>
+// 				<div className='avatar online'>
+// 					<div className='w-12 rounded-full'>
+// 						<img
+// 							src='https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png'
+// 							alt='user avatar'
+// 						/>
+// 					</div>
+// 				</div>
+
+// 				<div className='flex flex-col flex-1'>
+// 					<div className='flex gap-3 justify-between'>
+// 						<p className='font-bold text-gray-200'>John Doe</p>
+// 						<span className='text-xl'>🎃</span>
+// 					</div>
+// 				</div>
+// 			</div>
+
+// 			<div className='divider my-0 py-0 h-1' />
+// 		</>
+// 	);
+// };
+// export default Conversation;
